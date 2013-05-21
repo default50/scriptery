da31ce29a16d3fe1aa0860da8c76a5910f757a6c
Sebastian Cruz <default50@gmail.com>
2013-05-21 20:12:05 -0300

    Script to play sounds and notify the user when mentioned.

---
+++ skype-messages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script to play a sound and pop up system notifications on Linux when a certain regex matches a message's content.
+
+# Make regex case insensitive
+shopt -s nocasematch
+
+REGEX=".*(crux|seba.*|rati).*"
+PLAYER="paplay"
+SOUND="/usr/share/sounds/freedesktop/stereo/message.oga"
+
+if [[ "${1}" =~ $REGEX ]]; then
+	$PLAYER ${SOUND} &
+	notify-send "Mentioned!" "$2 mentioned you." -i skype
+fi
