3f0bede55bd84504f63d9703bf0d907961693744
Sebastian Cruz <default50@gmail.com>
2014-04-17 11:29:20 -0300

    Now supporting digest auth

--- torrent-grabber.sh
+++ torrent-grabber.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+CREDENTIALS="myuser:mypass"
 RTGUI_URL="http://macumba.macumba-net.com.ar/rtgui/control.php"
 POST_URL="$RTGUI_URL/control.php"
 
-/usr/bin/curl $POST_URL -F "uploadtorrent=@${1}"
+/usr/bin/curl --digest -u "${CREDENTIALS}" $POST_URL -F "uploadtorrent=@${1}"
 /usr/bin/firefox -new-tab $RTGUI_URL
