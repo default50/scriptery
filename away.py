9fbf24c1688eebaf781cf1914060edc6894f00d4
Sebastian Cruz <cruzseba@amazon.com>
2015-05-20 18:12:13 +0100

    Support for Empathy added.

--- away.py
+++ away.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-from gobject import MainLoop
 from dbus import SessionBus
 from dbus.mainloop.glib import DBusGMainLoop
 import subprocess
+from gi.repository import TelepathyGLib as Tp
+from gi.repository.GObject import MainLoop
 
 class Away:
     def __init__(self):
@@ -13,13 +14,17 @@ class Away:
         self.loop=MainLoop()
         self.bus.add_signal_receiver(self.catch,self.mem,self.dest)
     def catch(self,away):
+        am = Tp.AccountManager.dup()
+        am.prepare_async(None, lambda *args: loop.quit(), None)
         if away == 1: #Screensaver turned on
             print "Screen saver turned ON"
             subprocess.call("dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause", shell=True)
             subprocess.call("xchat -e -c AWAY", shell=True)
+            am.set_all_requested_presences(Tp.ConnectionPresenceType.OFFLINE, 'Offline', "")
         else: #Screensaver turned off
             subprocess.call("xchat -e -c BACK", shell=True)
             subprocess.call("dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause", shell=True)
+            am.set_all_requested_presences(Tp.ConnectionPresenceType.AVAILABLE, 'Available', "")
             print "Screen saver turned OFF"
 
 Away().loop.run()
