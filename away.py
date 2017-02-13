378f514acdd80abd1ca42286ef774eb0f3fbace9
Sebastian Cruz <cruzseba@amazon.com>
2017-02-13 15:01:27 +0000

    Only changes status in hexchat if running to avoid it being launched unwillingly.

--- away.py
+++ away.py
@@ -2,6 +2,7 @@
 import dbus
 from dbus.mainloop.glib import DBusGMainLoop
 import subprocess
+import psutil
 import gi
 gi.require_version('TelepathyGLib', '0.12')
 from gi.repository import TelepathyGLib as Tp  # noqa
@@ -18,6 +19,19 @@ class Away:
         self.bus.add_signal_receiver(self.catch, self.mem, self.dest)
         self.paused_before = False
 
+    def is_running(self, name):
+        processes = []
+        for p in psutil.process_iter():
+            try:
+                if p.name() == name:
+                    processes.append(p)
+            except psutil.Error:
+                pass
+        if processes:
+            return True
+        else:
+            return False
+
     def catch(self, away):
         am = Tp.AccountManager.dup()
         proxy_new = self.bus.get_object("org.mpris.MediaPlayer2.spotify",
@@ -30,22 +44,27 @@ class Away:
                                         'PlaybackStatus')
 
         if away == 1:  # Screensaver turned on
-            print("Screen saver turned ON and Spotify was: {} ".format(status))
+            print('INFO: Screen saver turned ON and Spotify was:'
+                  ' {}'.format(status))
             if status == 'Playing':
                 self.paused_before = False
                 event_manager.PlayPause()
             else:
                 self.paused_before = True
-            subprocess.call("hexchat -e -c AWAY", shell=True)
+            if self.is_running('hexchat'):
+                print('INFO: hexchat running, marking as AWAY')
+                subprocess.call("hexchat -e -c AWAY", shell=True)
             subprocess.call(
                 "ssh cruzseba-laptop.aka.amazon.com 'pmset displaysleepnow'",
                 shell=True)
             am.set_all_requested_presences(Tp.ConnectionPresenceType.OFFLINE,
                                            'Offline', "")
         else:  # Screensaver turned off
-            print(
-                "Screen saver turned OFF and Spotify was: {} ".format(status))
-            subprocess.call("hexchat -e -c BACK", shell=True)
+            print('INFO: Screen saver turned OFF and Spotify was:'
+                  ' {}'.format(status))
+            if self.is_running('hexchat'):
+                print('INFO: hexchat running, marking as BACK')
+                subprocess.call("hexchat -e -c BACK", shell=True)
             if status == 'Paused' and not self.paused_before:
                 event_manager.PlayPause()
             am.set_all_requested_presences(Tp.ConnectionPresenceType.AVAILABLE,
